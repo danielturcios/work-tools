1c686becc2cb458872f5887f9faef567a0248c37
danielturcios <danielturciosa@gmail.com>
2023-08-02 14:33:53 -0700

    xml parser configured. TODO: generate multiple dictionaries to be used in sploss file

--- Cableloss Converter/converter.py
+++ Cableloss Converter/converter.py
@@ -1,10 +1,25 @@
 import xml.etree.ElementTree as ET
 
-def parseTree (fileName):
-    tree = ET.parse(fileName)
+def get_tree (file_name):
+    #
+    tree = ET.parse(file_name)
+    return tree
+
+def get_root (tree):
     root = tree.getroot()
     return root
 
+def get_measurements(root, kword):
+    losses = {}
+    for measurements in root.findall(kword):
+        for measurement in measurements:
+            freq = measurement.find("Frequency").text
+            gain = measurement.find("Gain").text
+            gain = abs(float(gain))
+            losses[freq] = gain
+    return losses
+
 if __name__ == "__main__":
-    root = parseTree("Cableloss Converter\Calibration Files\AA2213_15cm_Flex_Ant1.xml")
-    print (root)
+    tree = get_tree("Cableloss Converter\Calibration Files\AA2213_15cm_Flex_Ant1.xml")
+    root = get_root(tree)
+    get_measurements(root, 'Measures')
